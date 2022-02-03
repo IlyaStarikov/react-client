79141bf38209946d879b54fde8cc764d9bb373e7
Ilya Starikov <i.starikov@dunice.net>
2022-02-03 17:50:43 +0300

    Sign in

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -9,8 +9,8 @@ function Auth({ type }) {
   const isAuth = type === authTypes.REGISTRATION;
   return (
     <div className="form">
-      <h1>{isAuth ? 'Signin' : 'Login'}</h1>
-      {isAuth && <input id="text" type="text" className="validate" placeholder="Login" />}
+      <h1>{isAuth ? 'Sign in' : 'Login'}</h1>
+      {isAuth && <input id="text" type="text" className="validate" placeholder="Nickname" />}
       <input id="email" type="email" className="validate" placeholder="Email" />
       <input id="password" type="password" className="validate" placeholder="Password" />
       <button className="btn waves-effect waves-light" type="submit" name="action">
