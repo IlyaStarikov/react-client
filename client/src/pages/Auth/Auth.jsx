e4b4f22852f3c24ca0b77cb1ae776b805e267d30
Ilya Starikov <i.starikov@dunice.net>
2022-02-04 16:12:10 +0300

    add formik

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,7 +1,8 @@
 import React, { memo } from 'react';
 import { string } from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
-// import { useNavigate } from 'react-router';
+import { Navigate } from 'react-router';
+import { Formik, Field, Form } from 'formik';
 
 import { authTypes } from '../../redux/constants';
 import { authLogin, authRegistration } from '../../redux/actions/actions';
@@ -11,7 +12,6 @@ import './Auth.css';
 function Auth({ type }) {
   const dispatch = useDispatch();
   const isAuth = type === authTypes.REGISTRATION;
-  // const navigate = useNavigate();
 
   const {
     fetching,
@@ -19,46 +19,34 @@ function Auth({ type }) {
     error,
   } = useSelector((state) => state.login);
 
-  const submitLogin = () => {
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
-    dispatch(authLogin({ email: email.value, password: password.value }));
+  const submitLogin = (values) => {
+    dispatch(authLogin(values));
   };
 
-  const submitRegistration = () => {
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
-    const name = document.getElementById('name');
-    const login = document.getElementById('nickname');
-    dispatch(authRegistration({
-      email: email.value,
-      password: password.value,
-      login: login.value,
-      name: name.value,
-    }));
+  const submitRegistration = (values) => {
+    dispatch(authRegistration(values));
   };
-
-  if (isLogin) {
-    return (
-      <>
-        <h3>{isLogin}</h3>
-        <h4>Go to news page</h4>
-      </>
-    );
-  }
 
   return (
     <div className="form">
       {fetching ? <h3>Wait a minute...</h3> : ''}
       {error ? <h4>{error}</h4> : ''}
       <h1>{isAuth ? 'Sign in' : 'Login'}</h1>
-      {isAuth && <input id="name" type="text" className="validate" placeholder="Name" />}
-      {isAuth && <input id="nickname" type="text" className="validate" placeholder="Nickname" />}
-      <input id="email" type="email" className="validate" placeholder="Email" />
-      <input id="password" type="password" className="validate" placeholder="Password" />
-      <button onClick={isAuth ? submitRegistration : submitLogin} className="btn waves-effect waves-light" type="submit" name="action">
-        {isAuth ? 'Register' : 'Login'}
-      </button>
+      <Formik
+        initialValues={{
+          name: '', email: '', login: '', password: '',
+        }}
+        onSubmit={isAuth ? submitRegistration : submitLogin}
+      >
+        <Form>
+          {isAuth && <Field name="name" type="text" className="validate" placeholder="Name" />}
+          {isAuth && <Field name="login" type="text" className="validate" placeholder="Nickname" />}
+          <Field name="email" type="email" className="validate" placeholder="Email" />
+          <Field name="password" type="password" className="validate" placeholder="Password" />
+          <button className="btn waves-effect waves-light" type="submit">{isAuth ? 'Register' : 'Login'}</button>
+        </Form>
+      </Formik>
+      {isLogin ? <Navigate to="/" /> : ''}
     </div>
   );
 }
