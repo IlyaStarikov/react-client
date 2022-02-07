43df406af52b80ef86c7389908cd880bdc5a5fcd
Ilya Starikov <i.starikov@dunice.net>
2022-02-07 18:03:56 +0300

    create userPage

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 
 import { logoutAction } from '../../redux/actions/actions';
 
+import './NavBar.css';
+
 function NavBar() {
   const dispatch = useDispatch();
   const userIsLogin = useSelector((state) => state.login.isLogin);
--- client/src/pages/User/User.jsx
+++ client/src/pages/User/User.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Card from '../../components/Card/Card';
+import logo from '../../assets/123.jpg';
+
+import './User.css';
 
 function User() {
-  return <div>User</div>;
+  const {
+    news: postsItems,
+  } = useSelector((state) => state.posts);
+
+  return (
+    <div className="container">
+      <div className="container__flex">
+        <div className="user">
+          <img className="avatar" alt="" src={logo} />
+          <p>User Name</p>
+          <p>User NickName</p>
+          <button id="button-edit" type="button" className="btn-floating btn-large waves-effect waves-light red">
+            <i className="material-icons">edit</i>
+          </button>
+          <p className="add">
+            Add News
+            <button type="button" className="btn-floating btn-large waves-effect waves-light red">
+              <i className="material-icons">add</i>
+            </button>
+          </p>
+        </div>
+        <div className="news">
+          {postsItems.map((post) => <Card post={post} key={post.id} />)}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default User;
